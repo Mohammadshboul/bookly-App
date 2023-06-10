5d437347fc43a01b3890e04cc33afa22f065e95b
MohammadALShboul99 <110593677+MohammadALShboul99@users.noreply.github.com>
2023-06-10 17:24:21 +0300

    add best seller list view item

--- lib/Features/home/presentation/views/widget/home_view_body.dart
+++ lib/Features/home/presentation/views/widget/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:booklyapp/Features/home/presentation/views/widget/custom_app_bar.dart';
 import 'package:booklyapp/Features/home/presentation/views/widget/featured_list_view.dart';
+import 'package:booklyapp/core/utils/assets.dart';
 import 'package:booklyapp/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -14,9 +15,33 @@ class HomeViewBody extends StatelessWidget {
         CustomAppBar(),
         FeaturedBooksListView(),
         SizedBox(height: 35,),
-        Text("Best Seller",style: Styles.titleMedium,)
+        Text("Best Seller",style: Styles.titleMedium,),
+        SizedBox(height: 25,),
+        BestSellerListViewItem(),
       ],
     );
   }
 }
 
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(height: 130,
+      child: Row(children: [
+       AspectRatio(
+        aspectRatio: 2.5 / 4,
+        child: Container(
+          decoration:  BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            image: const DecorationImage(
+                image: AssetImage(AssetsData.test), fit: BoxFit.fill),
+          ),
+        ),
+      ),
+         const Column(children: [],),
+      ],),
+    );
+  }
+}
