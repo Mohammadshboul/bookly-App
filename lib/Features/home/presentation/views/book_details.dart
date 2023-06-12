cc4cbd7a72d31dcc727560a63f3b5ebdd7ac77aa
MohammadALShboul99 <110593677+MohammadALShboul99@users.noreply.github.com>
2023-06-12 19:20:08 +0300

    craete book details

---
+++ lib/Features/home/presentation/views/book_details.dart
@@ -0,0 +1,13 @@
+import 'package:booklyapp/Features/home/presentation/views/widget/book_details_view_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsView extends StatelessWidget {
+  const BookDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BookDetailsViewBody(),
+    );
+  }
+}
