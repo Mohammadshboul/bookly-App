020d3290bbee0c864c491ae299cd4b956849828e
MohammadALShboul99 <110593677+MohammadALShboul99@users.noreply.github.com>
2023-06-20 21:19:56 +0300

    create featured books cubit state

---
+++ lib/Features/home/presentation/view_model/featured_books_cubit/featured_books_state.dart
@@ -0,0 +1,25 @@
+part of 'featured_books_cubit.dart';
+
+abstract class FeaturedBooksState extends Equatable {
+  const FeaturedBooksState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class FeaturedBooksInitial extends FeaturedBooksState {}
+
+class FeaturedBooksLoading extends FeaturedBooksState {}
+
+class FeaturedBooksSuccess extends FeaturedBooksState {
+  final List<BookModel> books;
+
+  const FeaturedBooksSuccess(this.books);
+}
+
+class FeaturedBooksfailure extends FeaturedBooksState {
+  final String errMessage;
+  const FeaturedBooksfailure(
+    this.errMessage,
+  );
+}
