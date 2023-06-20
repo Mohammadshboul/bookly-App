d92cedcebffd4965d8d6fb3885683d7bbe4ae850
MohammadALShboul99 <110593677+MohammadALShboul99@users.noreply.github.com>
2023-06-20 21:57:54 +0300

    newest books cubit done

--- lib/Features/home/presentation/view_model/newset_books_cubit/newset_books_cubit.dart
+++ lib/Features/home/presentation/view_model/newset_books_cubit/newset_books_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
-import 'package:booklyapp/Features/home/data/models/book_model/book_model.dart';
 import 'package:equatable/equatable.dart';
+
+import 'package:booklyapp/Features/home/data/models/book_model/book_model.dart';
+import 'package:booklyapp/Features/home/data/repos/home_repo.dart';
 
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchNewestBooks() async {
+    emit(NewsetBooksLoading());
+    var resulte = await homeRepo.fetchFeaturedBooks();
+    resulte.fold((failure) {
+      emit(NewsetBooksFailure(failure.errMessage));
+    }, (books) {
+      emit(NewsetBooksSuccess(books));
+    });
+  }
 }
