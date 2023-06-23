67c16d531fe64d378ef9721ccad7c9531fcd9c23
MohammadALShboul99 <110593677+MohammadALShboul99@users.noreply.github.com>
2023-06-23 16:37:31 +0300

    cached network image

--- lib/Features/home/presentation/views/widget/custom_book_item.dart
+++ lib/Features/home/presentation/views/widget/custom_book_item.dart
@@ -1,17 +1,19 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookImage extends StatelessWidget {
   const CustomBookImage({super.key, required this.imageUrl});
-final String imageUrl;
+  final String imageUrl;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.7 / 4,
-      child: Container(
-        decoration:  BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          image:  DecorationImage(
-              image:NetworkImage(imageUrl), fit: BoxFit.fill),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4,
+        child: CachedNetworkImage( 
+          imageUrl: imageUrl,
+          fit: BoxFit.fill,
+          errorWidget:(context, url, error) => const Icon(Icons.error) ,
         ),
       ),
     );
